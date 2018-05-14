72c1fb8adb2f7a0de5c5f5bbe39b79587435cb09
Mark Harrison <mharrisondesign@mail.com>
2018-05-14 13:05:35 +0000

    Addition of a new table called 'Friends'

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,26 +1,22 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 environment
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                            #removed '' from 'username'
-                            user=username,
-                            password='',
-                            db='Chinook')
-                            
+                             user=username,
+                             password='',
+                             db='Chinook')
+
 try:
-    # run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print (row)
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
-    # Close the connection, regardless of whether the above was successful
     connection.close()
-    
-    
-        
